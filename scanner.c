b8577c9a4f565a1d20bba5b2210ef18d00ed26ec
Jan Klhůfek <jan.klhufek@gmail.com>
2019-10-26 17:57:41 +0200

    Added files used for scanner.

---
+++ scanner.c
@@ -0,0 +1,9 @@
+/*
+ * @file scanner.c
+ * @brief Implementace lexikálního analyzátoru
+ * @author Jan Klhůfek (xklhuf01@stud.fit.vutbr.cz)
+ * @date XX.XX.2019
+ *
+ * Projekt: Implementace prekladace imperativního jazyka IFJ19 (varianta II)
+ * VUT FIT
+ */
---
+++ scanner.h
@@ -0,0 +1,19 @@
+/*
+ * @file scanner.h
+ * @brief Hlavičkový soubor pro lexikální analyzátor
+ * @author Jan Klhůfek (xklhuf01@stud.fit.vutbr.cz)
+ * @date XX.XX.2019
+ *
+ * Projekt: Implementace prekladace imperativního jazyka IFJ19 (varianta II)
+ * VUT FIT
+ */
+
+#ifndef SCANNER_H
+#define SCANNER_H
+
+
+
+
+
+
+#endif
