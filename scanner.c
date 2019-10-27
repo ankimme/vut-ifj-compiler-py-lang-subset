284f526b563d142bb470176e31b72c0a61adfc93
Jan Klhůfek <jan.klhufek@gmail.com>
2019-10-27 02:27:30 +0200

    Created structs and enum needed for scanner. Made initial structure of scanner.

--- scanner.c
+++ scanner.c
@@ -7,3 +7,101 @@
  * Projekt: Implementace prekladace imperativního jazyka IFJ19 (varianta II)
  * VUT FIT
  */
+
+//#include "error.h"
+#include "scanner.h"
+
+int new_line = 1;
+
+void Get_next_token(St_token *Token)
+{
+    char c = getchar();
+
+    if (new_line == 1)
+    {
+        int counter = 0;
+
+        while ((new_line == 1) && (c == ' '))
+        {
+            counter++;
+
+            c = getchar();
+        }
+        if (counter > 0)
+        {
+            //generovat indent/dedent
+            //zanoření/vynoření
+        }
+    }
+
+    //vytvoření stringu pro aktuální stav automatu
+    Dynamic_string *string;
+    if (!string_init(string))
+    {
+        //vnitřní chyba alokace, error 99
+    }
+    state = start;
+
+    while(c != EOF)
+    {
+        switch(state)
+        {
+            case start:
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_')) //identifikátor
+                {
+                    state = INDENTIFIER;
+                }
+                else if (c >= '1' && c <= '9') //celé číslo
+                {
+                    state = INTEGER;
+                }
+                else if (c == '#') //komentář
+                {
+                    state = commentary;
+                }
+                else if (c == '!') //vykřičník
+                {
+                    state = exlamation_mark;
+                }
+                else if (c == '<' || c == '>' || c == '=') //relační operátor
+                {
+                    state = RELATION_OPERATION_1;
+                }
+                else if (c == '/') //lomítko
+                {
+                    state = BINARY_OPERATION_1;
+                }
+                else if (c == '+' || c == '-' || c == '*') //binární operátor
+                {
+                    state = BINARY_OPERATION_3;
+                }
+                else if (c == '"') //uvozovka
+                {
+                    state = quotation_mark_1;
+                }
+                else if (c == '\'') //apostrof
+                {
+                    state = character;
+                }
+                else if (c == ' ' || c == '\t')
+                {
+                    state = start;
+                }
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+    //TODO
+    //Přidat dedenty a vynoření z tabulek symbolů
+    //Návratový kod
+}
+
+
--- scanner.h
+++ scanner.h
@@ -11,9 +11,79 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#include <stdio.h>
+//#include "dynamic_string.h"
+
+enum state
+{
+    start; //počáteční stav automatu
+
+    /*  IDENTIFIKÁTOR  */
+    IDENTIFIER; //koncový stav, typ indentifikátor
+
+    /*  CELÉ ČÍSLO  */
+    INTEGER; //koncový stav, typ celé číslo
+
+    /*  DESETINNÝ LITERÁL  */
+    decimal_point; //mezistav, desetinná čárka
+    DOUBLE_1; //koncový stav, typ desetinný literál
+
+    exponent; //mezistav, literál s exponentem
+    plus_minus; //mezistav, literál s exponentem
+    DOUBLE_2; //koncový stav, typ desetinný literál
+
+    /*  ŘETĚZCOVÝ LITERÁL  */
+    character; //mezistav, načítání znaků řetězce
+
+    escape; //mezistav, znak pro možnou escape sekvenci
+
+    hexadecimal_1; //mezistavy, escape sekvence pro platné hexadecimální číslo
+    hexadecimal_2; 
+
+    STRING_LITERAL; //koncový stav, typ řetězcový literál
+
+    /*  KOMENTÁŘ  */
+    commentary; //mezistav, komentář
+    END_OF_LINE; //koncový stav, typ znaku konce řádku
+
+    /*  RELAČNÍ OPERÁTOR  */
+    exclamation_mark; //mezistav, vykřičník
+    RELATION_OPERATOR_1; //koncové stavy, typ konkrétního relačního operátoru (<,>,=)
+    RELATION_OPERATOR_2; //(<=,>=,==,!=)
+
+    /*  BINÁRNÍ OPERÁTOR  */
+    BINARY_OPERATOR_1; //koncové stavy, typ různých binárních operátorů (/)
+    BINARY_OPERATOR_2; // (//)
+    BINARY_OPERATOR_3; // (+,-,*)
+
+    /*  DOKUMENTAČNÍ ŘETĚZEC  */
+    quotation_mark_1; //mezistavy, sekvence 3 po sobě jdoucích znaků uvozovek (""")
+    quotation_mark_2;
+    quotation_mark_3; //mezistav, slouží k načítání dalších znaků
+    slash; //mezistav, slouží k ošetření escape sekvence pro uvozovky (\")
+
+    ending_quotation_1; //mezistavy, posloupnost 3 ukončujících uvotovek (""")
+    ending_quotation_2;
+    DOCUMENTARY_LITERAL; //koncový stav, nemá přiřazen žádný typ
+}
 
 
+typedef union
+{
+    Dynamic_string *string; //Zkontrolvat
+    Keyword keyword; //Zkontrolvat
+    int number;
+    double decimal;
 
+}Token_value
+
+typedef struct
+{
+    Dynamic_string *type; //Zkontrolvat
+    Token_value attribute;
+}St_token;
+
+void Get_next_token(St_token* Token);
 
 
 #endif
