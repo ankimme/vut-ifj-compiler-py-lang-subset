d0eb2846c6a25fb1c4e9ecba14cac99856a7a1fb
Andrea Chimenti <andre.chimenti@gmail.com>
2019-11-28 19:27:22 +0100

    Updated documentation of dynamic string library

--- dynamic_string.h
+++ dynamic_string.h
@@ -2,6 +2,7 @@
  * @file dynamic_string.h
  * @brief Deklarace struktur a funkcí pro dynamic_string knihovnu
  * @author Matej Alexej Helc (xhelcm00@stud.fit.vutbr.cz)
+ * @author Andrea Chimenti (xchime00@stud.fit.vutbr.cz)
  * @date 27.10.2019
  *
  * Projekt: Implementace překladače imperativního jazyka IFJ19 (varianta II)
@@ -26,14 +27,16 @@ typedef struct
 /**
  * Vyprázdni reťazec.
  *
- * @param s Ukazeteľ na reťazec.
+ * @param s Ukazeteľ na strukturu dynamického reťazeca.
+ * @post Nastaví všechny znaky v alokované paměti pro řetězec na '\0'.
  */
 void string_clear(dynamic_string *s);
 
 /**
  * Inicializácia dynamického reťazca.
  *
- * @param s Ukazateľ na reťazec.
+ * @param s Ukazateľ na strukturu dynamického reťazeca.
+ * @pre Paměť pro pointer s je již alokována
  * @return 1 ak sa inicializácia podarila, 0 ak nie.
  */
 int string_init(dynamic_string *s);
@@ -41,26 +44,26 @@ int string_init(dynamic_string *s);
 /**
  * Uvoľňuje alokovanú pamäť pre dynamický reťazec.
  *
- * @param s Ukazateľ na reťazec.
+ * @param s Ukazateľ na strukturu dynamického reťazeca.
  */
 void string_free(dynamic_string *s);
 
 /**
  * Pridá znak na koniec dynamického reťazca.
  *
- * @param s Dynamický reťazec.
+ * @param s Ukazateľ na strukturu dynamického reťazeca.
  * @param c Znak, ktorý chceme pridať.
  * @return 1 ak sa podarilo pridať znak, 0 ak nie.
  */
 int string_add_char(dynamic_string *s, char c);
 
 /**
- * Zreťazí reťazce.
+ * Pridá reťazec na koniec dynamického reťazca.
  *
- * @param s Dynamický reťazec.
- * @param const_string Reťazec, konštanta.
+ * @param s Ukazateľ na strukturu dynamického reťazeca.
+ * @param string Reťazec ktorý se prilepí k reťazci s.
  * @return 1 ak sa zreťazenie podarilo, 0 ak nie.
  */
-int strings_append(dynamic_string *s, char *string);
+int string_append(dynamic_string *s, char *string);
 
 #endif
