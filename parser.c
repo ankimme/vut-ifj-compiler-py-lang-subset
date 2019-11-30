1b646a6afa174ffefe8077858539967fe01c6a23
Andrea Chimenti <andre.chimenti@gmail.com>
2019-11-30 02:44:24 +0100

    Started working on parser

--- parser.c
+++ parser.c
@@ -1,20 +1,254 @@
 /*
  * @file parser.c
  * @brief Implementace syntaktického analyzátoru
- * @author 
- * @date 28.11.2019
+ * @author Andrea Chimenti (xchime00@stud.fit.vutbr.cz)
+ * @date 29.11.2019
  *
  * Projekt: Implementace překladače imperativního jazyka IFJ19 (varianta II)
  * VUT FIT
  */
 
+#include "parser.h"
 
-
-
-
-int new_token(St_token* token, tStack* stack)
+int start_analysis()
 {
-    get_next_token(token, stack)
+    tParser_data *parser_data = malloc(sizeof(tParser_data));
+    if(!init_parser_data(parser_data))
+    {
+        return 99;
+    }
 
-    return token->error_value;
+    // nacteni prvniho tokenu
+    get_next_token(parser_data->current_token, parser_data->scanner_stack);
+    prog(parser_data);
+
+    int error_code = parser_data ? parser_data->error_code : 99;
+    free_parser_data(parser_data);
+    free(parser_data);
+    parser_data = NULL;
+
+    return error_code;
+}
+
+bool init_parser_data(tParser_data *parser_data)
+{
+    // inicializace tokenu
+    parser_data->current_token = malloc(sizeof(St_token));
+    if (parser_data == NULL)
+    {
+        return false;
+    }
+    parser_data->current_token->type = UNDEFINED;
+    parser_data->current_token->error_value = 0;
+    parser_data->current_token->attribute = malloc(sizeof(dynamic_string));
+    if(parser_data->current_token->attribute == NULL)
+    {
+        return false;
+    }
+    if(!string_init(parser_data->current_token->attribute))
+    {
+        return false;
+    }
+
+    // inicializace zasobniku pro scanner
+    parser_data->scanner_stack = malloc(sizeof(tStack));
+    tStack_init(parser_data->scanner_stack);
+    tStack_push(parser_data->scanner_stack, 0);
+
+    // inicializace error kodu
+    parser_data->error_code = 0;
+
+    // inicializace flagu unget_token
+    parser_data->unget_token = false;
+
+    return true;
+}
+
+void free_parser_data(tParser_data *parser_data)
+{
+    // uvolneni tokenu
+    string_free(parser_data->current_token->attribute);
+    free(parser_data->current_token->attribute);
+    parser_data->current_token->attribute = NULL;
+ 
+    free(parser_data->current_token);
+    parser_data->current_token = NULL;
+
+    // uvolneni zasobniku
+    free(parser_data->scanner_stack);
+    parser_data->scanner_stack = NULL;
+}
+
+bool get_token_and_set_error_code(tParser_data *parser_data)
+{
+    if (parser_data->unget_token)
+    {
+        parser_data->unget_token = false;
+        return true;
+    }
+    get_next_token(parser_data->current_token, parser_data->scanner_stack);
+    // zachyceni lexikalni chyby
+    if (parser_data->current_token->error_value)
+    {
+        parser_data->error_code = parser_data->current_token->error_value;
+        return false;
+    }
+    return true;
+}
+
+bool get_compare_check(tParser_data *parser_data, Token_type type)
+{
+    if(get_token_and_set_error_code(parser_data))
+    {
+        if (parser_data->current_token->type == type)
+        {
+            return true;
+        }
+        else
+        {
+            parser_data->error_code = 2;
+        }
+    }
+    return false;
+}
+
+bool prog(tParser_data *parser_data)
+{
+    // zachyceni jiz nastale chyby
+    if (parser_data->error_code)
+    {
+        return false;
+    }
+
+    // token je klicove slovo
+    if (parser_data->current_token->type == TOKEN_KEYWORD)
+    {
+        // token je "def" -> pravidlo 2
+        if (strcmp(parser_data->current_token->attribute->str, "def") == 0)
+        {
+            // musi nasledovat identifikator
+            if (!get_compare_check(parser_data, TOKEN_IDENTIFIER))
+            {
+                return false;
+            }
+
+            // musi nasledovat "("
+            if (!get_compare_check(parser_data, TOKEN_LEFT_BRACKET))
+            {
+                return false;
+            }
+
+            // musi nasledovat neterminal PARAMS
+            get_token_and_set_error_code(parser_data);
+            if (!params(parser_data))
+            {
+                return false;
+            }
+
+            // musi nasledovat ")"
+            if (!get_compare_check(parser_data, TOKEN_RIGHT_BRACKET))
+            {
+                return false;
+            }
+
+            // musi nasledovat ":"
+            if (!get_compare_check(parser_data, TOKEN_COLON))
+            {
+                return false;
+            }
+
+            // musi nasledovat EOL
+            if (!get_compare_check(parser_data, TOKEN_EOL))
+            {
+                return false;
+            }
+
+            // musi nasledovat INDENT
+            if (!get_compare_check(parser_data, TOKEN_INDENT))
+            {
+                return false;
+            }
+
+            // musi nasledovat neterminal STATEMETS
+            get_token_and_set_error_code(parser_data);
+            if (!statements(parser_data))
+            {
+                return false;
+            }
+
+            // musi nasledovat DEDENT
+            if (!get_compare_check(parser_data, TOKEN_DEDENT))
+            {
+                return false;
+            }
+
+            // musi nasledovat neterminal PROG
+            get_token_and_set_error_code(parser_data);
+            if (!prog(parser_data))
+            {
+                return false;
+            }
+        }
+        //TODO
+    }
+    // token je EOL
+    else if (parser_data->current_token->type == TOKEN_EOL)
+    {
+        // musi nasledovat neterminal PROG
+        get_token_and_set_error_code(parser_data);
+        if (!prog(parser_data))
+        {
+            return false;
+        }
+    }
+    // token je EOF
+    else if (parser_data->current_token->type == TOKEN_EOF)
+    {
+        return true;
+    }
+    // TODO
+    else
+    {
+        parser_data->error_code = 2;
+        return false;
+    }
+    
+    return true;
+}
+
+bool params(tParser_data *parser_data)
+{
+    // zachyceni jiz nastale chyby
+    if (parser_data->error_code)
+    {
+        return false;
+    }
+
+    // token je ")" -> pravidlo 6
+    if (parser_data->current_token->type == TOKEN_RIGHT_BRACKET)
+    {
+        parser_data->unget_token = true;
+        return true;
+    }
+    // TODO
+    else
+    {
+        parser_data->error_code = 2;
+        return false;
+    }
+    
+    return true;
+}
+
+bool statements(tParser_data *parser_data)
+{
+    // zachyceni jiz nastale chyby
+    if (parser_data->error_code)
+    {
+        return false;
+    }
+
+    get_token_and_set_error_code(parser_data); // DELETE - only for testing purposes
+    return true;
+    // TODO
 }
--- parser.h
+++ parser.h
@@ -1,8 +1,8 @@
 /*
  * @file parser.h
  * @brief Implementace syntaktického analyzátoru
- * @author 
- * @date 28.11.2019
+ * @author Andrea Chimenti (xchime00@stud.fit.vutbr.cz)
+ * @date 29.11.2019
  *
  * Projekt: Implementace překladače imperativního jazyka IFJ19 (varianta II)
  * VUT FIT
@@ -14,15 +14,85 @@
 #include "scanner.h"
 #include "symtable.h"
 
+#include <stdbool.h>
+
+typedef struct
+{
+    St_token *current_token;
+    tStack *scanner_stack;
+    int error_code;
+    bool unget_token;
+} tParser_data;
 
 
-int new_token(St_token* token, tStack* stack);
 
+/**
+ * Zajistí spuštění syntaktické analýzy a překladu vstupního kódu
+ *
+ * @return 0 v případě úspěchu, jiné číslo v případě chyby (podle zadání)
+ */
+int start_analysis();
 
+/**
+ * Inicializace struktury pro uchování dat pro parser
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @pre Paměť pro ukazatel je již alokována
+ * @return true v případě úspěchu, false v případě neúspěchu
+ */
+bool init_parser_data(tParser_data *parser_data);
 
+/**
+ * Uvolnění veškterých dat parseru
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ */
+void free_parser_data(tParser_data *parser_data);
 
+/**
+ * V případě že je unget_token true, nastaví unget_token na false a nenačíta další token
+ * V případě že je unget_token false, načte následující token a zkontroluje zda-li nedošlo k lexikální analýze. Pokud ano, nastaví error_code na 1 a vrací false
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @return true v případě úspěchu, false v případě chyby vzniklé při lexikální analýze
+ */
+bool get_token_and_set_error_code(tParser_data *parser_data);
 
-int prog();
+/**
+ * Načte následující token (záleží na nastavení flagu unget_token) a porovná typ načteného tokenu s typem předaným parametrem
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @param type Enum hodnota pro porovnání typu tokenu
+ * @post V případě neočekávaného typu tokenu, nastaví error_code na 2 (syntaktická chyba)
+ * @return true v případě že úspěchu, false v případě chyby
+ */
+bool get_compare_check(tParser_data *parser_data, Token_type type);
 
+/**
+ * Rozgenerovává neterminál PROG
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @post V případě chyby, nastaví příslušný error code
+ * @return true v případě úspěšného rozgenerování, false v případě chyby
+ */
+bool prog(tParser_data *parser_data);
+
+/**
+ * Rozgenerovává neterminál PARAMS
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @post V případě chyby, nastaví příslušný error code
+ * @return true v případě úspěšného rozgenerování, false v případě chyby
+ */
+bool params(tParser_data *parser_data);
+
+/**
+ * Rozgenerovává neterminál STATEMENTS
+ *
+ * @param parser_data Ukazel na struktu tParser_data
+ * @post V případě chyby, nastaví příslušný error code
+ * @return true v případě úspěšného rozgenerování, false v případě chyby
+ */
+bool statements(tParser_data *parser_data);
 
 #endif
