ed0f4611330e28870b52df3b9c3c5575cf5a3e8a
Andrea Chimenti <andre.chimenti@gmail.com>
2019-12-06 05:00:21 +0100

    Updated symtable tests

--- test/symtable_test.c
+++ test/symtable_test.c
@@ -17,25 +17,49 @@ int main()
 
     st_init(&symtable);
 
-    tData data;
+    // tHash_Table_Item htitem;
+        
+    tNodeData *data = malloc(sizeof(tNodeData));
+    data->a = 1;
+    data->lptr = NULL;
+    data->rptr = NULL;
 
-    data.number = 1;
-    st_insert_entry_in_current_context(&symtable, "adda", data);
-    data.number = 2;
-    st_insert_entry_in_current_context(&symtable, "daad", data);
-    data.number = 3;
-    st_insert_entry_in_current_context(&symtable, "ide3", data);
-    data.number = 4;
-    st_insert_entry_in_current_context(&symtable, "ide4", data);
-    data.number = 5;
-    st_insert_entry_in_current_context(&symtable, "ide5", data);
-    data.number = 6;
-    st_insert_entry_in_current_context(&symtable, "ide6", data);
-    data.number = 7;
-    st_insert_entry_in_current_context(&symtable, "ide7", data);
+    st_insert_entry_in_current_context_random_key(&symtable, data, HT_TYPE_NODE);
+
+    data = malloc(sizeof(tNodeData));
+    data->a = 2;
+    data->lptr = NULL;
+    data->rptr = NULL;
+
+    dynamic_string *str = malloc(sizeof(dynamic_string));
+    string_init(str);
+    string_append(str, "shell");
+
+    st_insert_entry_in_current_context(&symtable, str, data, HT_TYPE_NODE);
+
+    tHash_Table_Item *item = st_search_entry(&symtable, str);
+
+    printf("%i\n", item->node_data->a);
+
+    // st_insert_entry_in_current_context(&symtable, "k1", data, HT_TYPE_NODE);
 
 
-    tData* new_data;
+    // data.number = 1;
+    // data.number = 2;
+    // st_insert_entry_in_current_context(&symtable, "daad", data);
+    // data.number = 3;
+    // st_insert_entry_in_current_context(&symtable, "ide3", data);
+    // data.number = 4;
+    // st_insert_entry_in_current_context(&symtable, "ide4", data);
+    // data.number = 5;
+    // st_insert_entry_in_current_context(&symtable, "ide5", data);
+    // data.number = 6;
+    // st_insert_entry_in_current_context(&symtable, "ide6", data);
+    // data.number = 7;
+    // st_insert_entry_in_current_context(&symtable, "ide7", data);
+
+
+    // tData* new_data;
     /*
     new_data = st_search_entry(&symtable, "ide1");
     if (new_data)
@@ -80,73 +104,73 @@ int main()
         printf("NULL\n");
     */
 
-    st_indent(&symtable);
+    // st_indent(&symtable);
 
-    data.number = 8;
-    st_insert_entry_in_current_context(&symtable, "ide8", data);
+    // data.number = 8;
+    // st_insert_entry_in_current_context(&symtable, "ide8", data);
 
-    st_indent(&symtable);
+    // st_indent(&symtable);
 
-    data.number = 9;
-    st_insert_entry_in_current_context(&symtable, "ide9", data);
+    // data.number = 9;
+    // st_insert_entry_in_current_context(&symtable, "ide9", data);
 
-    new_data = st_search_entry(&symtable, "ide7");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide7");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide8");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide8");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide9");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide9");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    st_dedent(&symtable);
+    // st_dedent(&symtable);
 
-    new_data = st_search_entry(&symtable, "ide7");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide7");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide8");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide8");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide9");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide9");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    st_dedent(&symtable);
+    // st_dedent(&symtable);
 
-    new_data = st_search_entry(&symtable, "ide7");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide7");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide8");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide8");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
-    new_data = st_search_entry(&symtable, "ide9");
-    if (new_data)
-        printf("%i\n", new_data->number);
-    else
-        printf("NULL\n");
+    // new_data = st_search_entry(&symtable, "ide9");
+    // if (new_data)
+    //     printf("%i\n", new_data->number);
+    // else
+    //     printf("NULL\n");
 
     st_clean_all(&symtable);
 
