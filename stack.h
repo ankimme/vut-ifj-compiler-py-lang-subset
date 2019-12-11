62c2f968b34222a42cda40f09f2ab901b125eecf
Andrea Chimenti <andre.chimenti@gmail.com>
2019-12-11 21:13:59 +0100

    Stack final commit.

--- stack.h
+++ stack.h
@@ -24,33 +24,51 @@ typedef struct
 
 /* ZASOBNIK CELYCH CISEL */
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Inicializuje zásobník
+ * 
+ * @param s Ukazatel na strukturu zásobníku
+ * @pre Paměť pro zásobník už je alokovaná
  */
 void tStack_init ( tStack* s );
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Určí zda-li je zásobník prázný
+ * 
+ * @param s Ukazatel na strukturu zásobníku
+ * @return true v případě že je zásobník prázdný, jinak false
  */
 int tStack_empty ( const tStack* s );
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Určí zda-li je zásobník plný
+ * 
+ * @param s Ukazatel na strukturu zásobníku
+ * @return true v případě že je zásobník plný, jinak false
  */
 int tStack_full ( const tStack* s );
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Odstraní položku na vrcholu zásobníku
+ * 
+ * @param s Ukazatel na strukturu zásobníku
  */
 void tStack_pop ( tStack* s );
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Vrátí položku na vrcholu zásobníku
+ * 
+ * @param s Ukazatel na strukturu zásobníku
+ * @param i Ukazatel na integer
+ * @post V proměnné i je hodnota z vrcholu zásobníku
  */
 void tStack_top ( const tStack* s, int* i );
 
-/*
- * MISSING DOCUMENTATION!
+/**
+ * Vloží položku na vrchol zásobníku
+ * 
+ * @param s Ukazatel na strukturu zásobníku
+ * @param i Položka která se bude na zásobník vkládat
  */
 void tStack_push ( tStack* s, int i );
 
